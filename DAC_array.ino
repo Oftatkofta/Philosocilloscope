7657e51c3b7d83739242161cfa6c0f19668e0aba
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-02-16 22:27:11 +0100

    kinda working

--- DAC_array.ino
+++ DAC_array.ino
@@ -7,14 +7,14 @@ byte Ypins[] = {
   12, 13, A5, A4, A3, A2, A1, A0
 };
 int d;
-byte coordinateX[8];
-byte coordinateY[8];
-int shapeX[] = {0,37,127,219,255,219,127,37};
-int shapeY[] = {127,37,0,37,127,219,255,219};
+bool coordinateX[8];
+bool coordinateY[8];
+int shapeX[128];
+int shapeY[128];
 
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
   d = 0;
 
   // for loop to initialize each pin as an output:
@@ -22,12 +22,12 @@ void setup() {
     pinMode(Xpins[i], OUTPUT);
     pinMode(Ypins[i], OUTPUT);
   }
-//  for (int i = 0; i > sizeof(shapeX - 1); i++) {
-//    float x = 128*cos(i) + 127;
-//    float y = 128*sin(i) + 127;
-//    shapeY[i] = (int)i;
-//    shapeX[i] = (int)i;
-//  }
+    for (int i = 0; i < sizeof(shapeX - 1); i++) {
+      float x = (128*cos(i) + 127);
+      float y = (128*sin(i) + 127);
+      shapeY[i] = (int)y;
+      shapeX[i] = (int)x;
+    }
 
 }
 
@@ -42,17 +42,16 @@ void loop() {
   painter(coordinateX, Xpins, coordinateY, Ypins);
 
 
-  //delay(1);
-  // delay(200);
-  Serial.print(d);
-  Serial.print(" ");
-  for (int i = 0; i < 8; i++) {
-    Serial.print(coordinateX[i]);
-    Serial.print(" ");
-    Serial.print(shapeX[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+  delay(1);
+     //Serial.println(shapeX);
+  //  Serial.print(" ");
+  //  for (int i = 0; i < 8; i++) {
+  //    Serial.print(shapeX[i]);
+  //    Serial.print("=");
+  //    Serial.print(coordinateX[i]);
+  //    Serial.print(" ");
+  //  }
+  //  Serial.println();
   //tester(Xpins, Ypins);
   d++;
 }
@@ -70,27 +69,31 @@ void tester(byte pinArrayX[], byte pinArrayY[]) {
     digitalWrite(pinArrayY[i], LOW);
   }
 }
-void DigitToBinArray(int d, byte array[]) {
+void DigitToBinArray(int d, boolean array[]) {
   //Function that generates a 8-bit array from an int (LSB->MSB, like the DAC pins)
 
   d = d % 256; //Make sure that d does not overflow our 8-bit DAC
   for (int i = 0; i < 8; i++) {
-    array[i] = d % 2;
+    if (d % 2 == 1) {
+      array[i] = 1;
+    } else {
+      array[i] = 0;
+    }
     d = d / 2;
   }
 }
 
-void painter(byte coordinateX[], byte pinArrayX[], byte coordinateY[], byte pinArrayY[]) {
+void painter(bool coordinateX[], byte pinArrayX[], bool coordinateY[], byte pinArrayY[]) {
 
   for (int i = 0; i < 8; i++) {
-    if (coordinateX[i] == 1) {
+    if (coordinateX[i]) {
       digitalWrite(pinArrayX[i], HIGH);
     }
     else {
       digitalWrite(pinArrayX[i], LOW);
     }
 
-    if (coordinateY[i] == 1) {
+    if (coordinateY[i]) {
       digitalWrite(pinArrayY[i], HIGH);
     }
     else {
@@ -100,5 +103,23 @@ void painter(byte coordinateX[], byte pinArrayX[], byte coordinateY[], byte pinA
   //delayMicroseconds(10);
 }
 
+void painter2(bool coordinateX[], byte pinArrayX[], bool coordinateY[], byte pinArrayY[]) {
 
+  for (int i = 0; i < 8; i++) {
+    if (coordinateX[i]) {
+      digitalWrite(pinArrayX[i], HIGH);
+    }
+    else {
+      digitalWrite(pinArrayX[i], LOW);
+    }
+
+    if (coordinateY[i]) {
+      digitalWrite(pinArrayY[i], HIGH);
+    }
+    else {
+      digitalWrite(pinArrayY[i], LOW);
+    }
+  }
+  //delayMicroseconds(10);
+}
 
