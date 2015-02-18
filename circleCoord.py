3abb88379c157dfab51863d03e95ff05e78232be
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-02-18 20:38:59 +0100

    Works with predefined coordinates

--- circleCoord.py
+++ circleCoord.py
@@ -1,6 +1,10 @@
-f = open('circleCoord.csv',mode='rw')
+import math
 
-for i in range(0,360):
-    
+f = open('circleCoord.txt',mode='w')
+
+for i in range(0,360,2):
+    alpha=math.radians(i)
+    y = int(128*math.sin(alpha)+127.5)
+    f.write(str(y)+',')
 
 
