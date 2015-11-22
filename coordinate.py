6518451fb02c59b4497e6449d5b0b38b13af6564
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-11-22 20:30:59 +0100

    object method naming now following PEP8

--- coordinate.py
+++ coordinate.py
@@ -1,6 +1,6 @@
-__author__ = 'jens_e'
 import math
 import string
+import numpy as np
 
 
 class Point(object):
@@ -22,21 +22,21 @@ class Point(object):
         self.x = x
         self.y = y
 
-    def getX(self):
+    def get_x(self):
         """
         Getter method for a Coordinate object's x coordinate.
         """
         return self.x
 
-    def getY(self):
+    def get_y(self):
         # Getter method for a Coordinate object's y coordinate
         return self.y
 
     def __str__(self):
-        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+        return '<' + str(self.get_x()) + ',' + str(self.get_y()) + '>'
 
     def __eq__(self, other):
-        if (self.getX() == other.getX()) and (self.getY() == other.getY()):
+        if (self.get_x() == other.get_x()) and (self.get_y() == other.get_y()):
             return True
         return False
 
@@ -58,67 +58,73 @@ class Shape(Point):
     def __repr__(self):
         return 'Shape at(' + str(self.x) + ', ' + str(self.y) + ')'
 
-    def getNpoints(self):
+    def get_n_points(self):
 
         return len(self.points)
 
-    def addPoint(self, point):
+    def add_point(self, point):
 
         self.points.append(point)
 
-        if self.getNpoints() == 1:
-            self.max_x, self.min_x = point.getX(), point.getX()
-            self.max_y, self.min_y = point.getY(), point.getY()
+        if self.get_n_points() == 1:
+            self.max_x, self.min_x = point.get_x(), point.get_x()
+            self.max_y, self.min_y = point.get_y(), point.get_y()
 
-        if point.getX() > self.max_x:
-            self.max_x = point.getX()
+        if point.get_x() > self.max_x:
+            self.max_x = point.get_x()
 
-        if point.getY() > self.max_y:
-            self.max_y = point.getY()
+        if point.get_y() > self.max_y:
+            self.max_y = point.get_y()
 
-        if point.getX() < self.min_x:
-            self.min_x = point.getX()
+        if point.get_x() < self.min_x:
+            self.min_x = point.get_x()
 
-        if point.getY() < self.min_y:
-            self.min_y = point.getY()
+        if point.get_y() < self.min_y:
+            self.min_y = point.get_y()
 
-    def addPoints(self, pointlist):
         self.points.append(pointlist)
+    def add_points(self, pointlist):
 
-    def getShapeType(self):
+    def get_shape_type(self):
         return self.shape_type
 
-    def getPoints(self):
+    def get_points(self):
+        """
+
+        :return: (list) pointlist
+        """
         return self.points
 
-    def getSortedPoints(self):
+    def get_sorted_points(self):
         """
-        :return: (list) Points sorted on distance from origo (0,0)
+        :return:
+        (list) Points sorted on distance from Origo (0,0)
         """
 
         return sorted(self.points, key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
 
-    def sortPoints(self):
-        """
-        Sorts the pointlist in place
+    def sort_points(self):
+        """ Sorts the pointlist in place on distance from Origo (0,0)
         :return:
         None
         """
         self.points.sort(key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
 
-    def getPointsAsArray(self, variablename, ):
+    def get_points_as_C_array(self, variablename, ):
         """
 
-        :return: (str) Points as a C-array consisting of 16-bit words with x as MSB
+        :return: (str) Points as a C-array consisting of 16-bit words with x as MSB ends with newline
         """
         out = "uint16_t %s[] = {" %variablename
-        for point in self.getSortedPoints():
+        for point in self.get_sorted_points():
             "Constrains values to 8-bit"
-            x = max(min(int(round(point.getX())), 255), 0)
-            y = max(min(int(round(point.getY())), 255), 0)
+            x = max(min(int(round(point.get_x())), 255), 0)
+            y = max(min(int(round(point.get_y())), 255), 0)
             out += str((x << 8) + y) + ', '
         out = string.rstrip(out,', ')
-        return out + '};'
+        return out + '};\n'
+
+    def get_points_as_numpy_array(self):
 
 
 class Circle(Shape):
@@ -137,7 +143,7 @@ class Circle(Shape):
         for i in range(self.npoints):
             x = self.radius * math.cos(alpha) + self.x
             y = self.radius * math.sin(alpha) + self.y
-            self.addPoint(Point(x, y))
+            self.add_point(Point(x, y))
             alpha += alpha
 
 
@@ -149,25 +155,37 @@ class Square(Shape):
         self._coordgen()
 
     def _coordgen(self):
-        x0=self.x-self.width/2.0
-        x1=self.x+self.width/2.0
-        y0=self.y-self.height/2.0
-        y1=self.y+self.height/2.0
-        n_hpoints=int(round(float(self.width)/(self.width+self.height)*self.npoints))
-        n_vpoints=int(round(float(self.height)/(self.width+self.height)*self.npoints))
-        dx=self.width/(n_hpoints/2.0)
-        dy=self.height/(n_vpoints/2.0)
+        """
+        Adds roughly
 
-        for i in range(n_hpoints/2):
+        """
+        #TODO make npoints exact!
+        #Calculate corner points
+        x0, x1 = self.x-self.width*0.5, self.x+self.width*0.5
+        y0, y1 =self.y-self.height*0.5, self.y+self.height*0.5
+        #Calculate number of points per side, rounds to nearest whole number
+        n_hpoints=round(float(self.width)/(self.width+self.height)*0.5*self.npoints)
+        n_vpoints=round(float(self.height)/(self.width+self.height)*0.5*self.npoints)
+        #Calculate distnce between points
+        dx, dy =self.width/n_hpoints, self.height/n_vpoints
+
+        xlist=[]
+        ylist=[]
+        for i in range(int(n_hpoints)):
             x = x0+i*dx
-            p0 = Point(x, y0)
-            p1 = Point(x, y1)
-            self.addPoint(p0)
-            self.addPoint(p1)
+            if x > x1:
+                break
+            xlist.append(Point(x, y0))
+            xlist.append(Point(x, y1))
 
-        for i in range(n_vpoints/2):
+
+        for i in range(int(n_vpoints)):
             y=y0+i*dy
-            p0 = Point(x0, y0+i*dx)
-            p1 = Point(x1, y0+i*dy)
-            self.addPoint(p0)
-            self.addPoint(p1)
+            if y > y1:
+                break
+            ylist.append(Point(x0, y0+i*dx))
+            ylist.append(Point(x1, y0+i*dy))
+
+        self.points = self.points+xlist+ylist
+
+
--- points_tester.py
+++ points_tester.py
@@ -5,16 +5,21 @@ from coordinate import *
 import math
 import random
 
-#fout=open('DAC_shiftreg/shiftregDAC/test.ino', 'w')
+fout=open('DAC_shiftreg/shiftregDAC/test.ino', 'w')
 
+n=171
+print "N is %d" %n
+c=Circle(128, 128, 100, n)
+print "%s has %d points" %(c.get_shape_type(), c.get_n_points())
+print "minX: %s, maxX: %s, minY: %s, maxY: %s" %(c.min_x, c.max_x, c.min_y, c.max_y)
 
-c=Circle(128, 128, 100, 100)
-s=Square(128,128,45,90,100)
-print "%s has %d points" %(s.getShapeType(), s.getNpoints())
+s=Square(127,127,100,50,n)
+print "%s has %d points" %(s.get_shape_type(), s.get_n_points())
 print "minX: %s, maxX: %s, minY: %s, maxY: %s" %(s.min_x, s.max_x, s.min_y, s.max_y)
 
-print s.points
-print s.getSortedPoints()
-print s.getPointsAsArray('test')
-
-#fout.write()
+print s.get_points()
+print s.get_sorted_points()
+s.sort_points()
+sq = s.get_points_as_C_array('test1')
+ci = c.get_points_as_C_array('test2')
+fout.write(sq+ci)
