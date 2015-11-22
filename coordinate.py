e1537c0be01372397137a66bfa6a4b3e6649ec85
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-11-23 00:12:43 +0100

    getNumpy array implemented
    Better getter methods for base Pint class and tons of bug fixes.

--- coordinate.py
+++ coordinate.py
@@ -32,6 +32,36 @@ class Point(object):
         # Getter method for a Coordinate object's y coordinate
         return self.y
 
+    def get_rounded_x(self):
+        """
+
+        :return:(int) x coordinate rounded to nearest whole number.
+        """
+        return int(round(self.x))
+
+    def get_rounded_y(self):
+        """
+
+        :return: (int) y coordinate rounded to nearest whole number.
+        """
+        return int(round(self.y))
+
+    def get_constrained_x(self):
+        """
+
+        :return: (int) x coodinate constrained to 8-bit range 0-255
+        """
+        return max(min(self.get_rounded_x(), 255), 0)
+
+
+    def get_constrained_y(self):
+        """
+
+        :return: (int) y coodinate constrained to 8-bit range 0-255
+        """
+        return max(min(self.get_rounded_y(), 255), 0)
+
+
     def __str__(self):
         return '<' + str(self.get_x()) + ',' + str(self.get_y()) + '>'
 
@@ -82,10 +112,20 @@ class Shape(Point):
         if point.get_y() < self.min_y:
             self.min_y = point.get_y()
 
-        self.points.append(pointlist)
+
     def add_points(self, pointlist):
+        """
+        :param pointlist: (list) list of Point objects to add to the shape
+        :return: None
+        """
+
+        self.points += pointlist
 
     def get_shape_type(self):
+        """
+
+        :return: (str) Type of shape
+        """
         return self.shape_type
 
     def get_points(self):
@@ -101,30 +141,43 @@ class Shape(Point):
         (list) Points sorted on distance from Origo (0,0)
         """
 
-        return sorted(self.points, key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
+        return sorted(self.points,
+                      key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
 
     def sort_points(self):
         """ Sorts the pointlist in place on distance from Origo (0,0)
         :return:
         None
         """
-        self.points.sort(key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
+        self.points.sort(
+            key=lambda point: math.sqrt(point.x ** 2 + point.y ** 2))
 
     def get_points_as_C_array(self, variablename, ):
         """
 
-        :return: (str) Points as a C-array consisting of 16-bit words with x as MSB ends with newline
+        :return: (str) Points as a C-array consisting of 16-bit words
+        with x as MSB ends with newline
         """
         out = "uint16_t %s[] = {" %variablename
         for point in self.get_sorted_points():
             "Constrains values to 8-bit"
-            x = max(min(int(round(point.get_x())), 255), 0)
-            y = max(min(int(round(point.get_y())), 255), 0)
+            x = point.get_constrained_x()
+            y = point.get_constrained_y()
             out += str((x << 8) + y) + ', '
         out = string.rstrip(out,', ')
         return out + '};\n'
 
     def get_points_as_numpy_array(self):
+        """
+        :return: (numpy array) a 256x256 binary numpy array representing
+        the shape on screen, xy coordinates are cropped to 8-bit range (0-255)
+        """
+
+
+        out = np.zeros((256,256),dtype = bool)
+        for p in self.get_points():
+            out[p.get_constrained_y(), p.get_constrained_x()] = True
+        return out
 
 
 class Circle(Shape):
@@ -135,16 +188,13 @@ class Circle(Shape):
 
     def _coordgen(self):
 
-        if self.npoints > 1000:
-            alpha = long(2L * math.pi / self.npoints)
-        else:
-            alpha = float(2 * math.pi / self.npoints)
+        alpha = 2 * math.pi / self.npoints
 
         for i in range(self.npoints):
-            x = self.radius * math.cos(alpha) + self.x
-            y = self.radius * math.sin(alpha) + self.y
+            x = self.radius *  math.cos(alpha*i) + self.x
+            y = self.radius *  math.sin(alpha*i) + self.y
             self.add_point(Point(x, y))
-            alpha += alpha
+
 
 
 class Square(Shape):
--- points_tester.py
+++ points_tester.py
@@ -4,22 +4,36 @@ __author__ = 'jens_e'
 from coordinate import *
 import math
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
-fout=open('DAC_shiftreg/shiftregDAC/test.ino', 'w')
 
-n=171
+#fout=open('DAC_shiftreg/shiftregDAC/test.ino', 'w')
+
+n=40
 print "N is %d" %n
-c=Circle(128, 128, 100, n)
+c=Square(127, 127, 64,64, n*16)
 print "%s has %d points" %(c.get_shape_type(), c.get_n_points())
 print "minX: %s, maxX: %s, minY: %s, maxY: %s" %(c.min_x, c.max_x, c.min_y, c.max_y)
+s = Shape(127,127, shape_type='complex')
+for p in c.get_points():
+    sc = Circle(p.get_x(), p.get_y(), 64, n)
+    s.add_points(sc.get_points())
 
-s=Square(127,127,100,50,n)
 print "%s has %d points" %(s.get_shape_type(), s.get_n_points())
 print "minX: %s, maxX: %s, minY: %s, maxY: %s" %(s.min_x, s.max_x, s.min_y, s.max_y)
 
-print s.get_points()
-print s.get_sorted_points()
-s.sort_points()
-sq = s.get_points_as_C_array('test1')
-ci = c.get_points_as_C_array('test2')
-fout.write(sq+ci)
+
+
+
+
+#Z = np.logical_or(c.get_points_as_numpy_array(), s.get_points_as_numpy_array())
+Z = s.get_points_as_numpy_array()
+G = np.zeros((256,256,3))
+G[Z>0.5] = [0,1,0]
+G[Z<0.5] = [0,0,0]
+
+plt.imshow(G)
+plt.gca().invert_yaxis()
+plt.show()
+#fout.write(sq+ci)
