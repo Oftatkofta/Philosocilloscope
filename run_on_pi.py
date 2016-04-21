305abae8208974007136a2d1563d6649e6ee3dfc
oftatkofta <pi@raspberrypi.(none)>
2016-04-21 22:15:56 +0200

    testing from Pi

--- run_on_pi.py
+++ run_on_pi.py
@@ -1,24 +1,25 @@
 import RPi.GPIO as GPIO
 
 """
-MR = GPOI18; Master Clear, active low
-DS = GPIO17; Serial data input
-OE = GPIO27; Output Enable, active low
-ST_CP = GPIO22; latch
-SH_CP = GPIO4; clock
+MR = GPOI18, 12; Master Clear, active low
+DS = GPIO17, 11; Serial data input
+OE = GPIO27, 13; Output Enable, active low
+ST_CP = GPIO22, 15; latch
+SH_CP = GPIO4, 7; clock
 """
 
 GPIO.setmode(GPIO.BOARD)
-masterClearPin = 18
-outputEnablePin = 27
-serialDataPin = 17
-latchPin = 22
-clockPin = 4
+masterClearPin = 12
+outputEnablePin = 13
+serialDataPin = 11
+latchPin = 15
+clockPin = 7
 
 GPIO.setup(masterClearPin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(outputEnablePin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup([serialDataPin, latchPin, clockPin], GPIO.OUT, initial=GPIO.LOW)
 
-print GPIO.RPI_INFO
+#print GPIO.RPI_INFO
 
 
 GPIO.cleanup()
