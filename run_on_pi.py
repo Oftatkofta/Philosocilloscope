ec67ff588e4c1f23469bad93593341a5eac61a6d
oftatkofta <pi@raspberrypi.(none)>
2016-04-26 00:05:40 +0200

    IT FINALLY WORKS!

--- run_on_pi.py
+++ run_on_pi.py
@@ -9,11 +9,12 @@ import time
 from coordinate import *
 
 """
-MR = GPIO18, 12; Master Clear, active low
-DS = GPIO17, 11; Serial data input
-OE = GPIO27, 13; Output Enable, active low
-ST_CP = GPIO22, 15; latch 22pF ceramic capacitor to GND
-SH_CP = GPIO4, 7; clock
+MR = GPIO18, pin12; Master Clear, active low
+DS = GPIO17, pin11; Serial data input
+OE = GPIO27, pin13; Output Enable, active low
+ST_CP = GPIO22, pin15; latch 22pF ceramic capacitor to GND
+SH_CP = GPI23, pin16; clock
+Q7' = utput from last register, connected to GPIOx, pinX to
 """
 
 if not dryrunFlag:
@@ -22,7 +23,7 @@ if not dryrunFlag:
     outputEnablePin = 13
     serialDataPin = 11
     latchPin = 15
-    clockPin = 7
+    clockPin = 16
     GPIO.setwarnings(False) #For ease of debugging
     GPIO.setup(masterResetPin, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(outputEnablePin, GPIO.OUT, initial=GPIO.LOW)
@@ -32,13 +33,13 @@ if not dryrunFlag:
 
 def reset():
     GPIO.output(masterResetPin, 0)
-    time.sleep(0.001)
+    #time.sleep(0.00001)
     GPIO.output(masterResetPin, 1)
-    GPIO.output(outputEnablePin, 0)
+    #GPIO.output(outputEnablePin, 0)
 
 def latch():
     GPIO.output(latchPin, 1)
-    time.sleep(0.001)
+    #time.sleep(0.001)
     GPIO.output(latchPin, 0)
 
 def shiftOut(Point):
@@ -51,18 +52,19 @@ def shiftOut(Point):
     Returns: None
 
     """
-    reset()
+    #reset()
     x=Point.get_constrained_x()
     y=Point.get_constrained_y()
     binary_representation = bin(x)[2:].zfill(8)+bin(y)[2:].zfill(8)
 
 
    
-    print("shifting: ", binary_representation)
+    #print("shifting: ", binary_representation)
     for bit in binary_representation:
         GPIO.output(serialDataPin, int(bit))
+        #time.sleep(0.00001)
         GPIO.output(clockPin, 1)
-        time.sleep(0.001)
+        #time.sleep(0.001)
         #latch()
         #GPIO.output(serialDataPin, int(bit))
         GPIO.output(clockPin, 0)
@@ -76,34 +78,28 @@ def shiftOut(Point):
 
 
 #print GPIO.RPI_INFO
-
-def funkyFunction(npoints):
-    p0 = Point(0,0)
-    p1 = Point(0,255)
-    p2 = Point(255, 255)
-    p3 = Point(255, 0)
-
-    p4 = Point(0,127)
-    p5 = Point(127,255)
-    p6 = Point(255, 127)
-    p7 = Point(127,0)
-
-
-    l1 = Line(p1,p2,npoints)
-    l2 = Line(p0,p3,npoints)
-    l3 = Line(p0,p1,npoints)
-    l4 = Line(p3,p2,npoints)
-
-    l=Circle(Point(127,127),127,npoints)
-
-    return l
-
-square = funkyFunction(200)
-
-for x in range(255,0,-10):
-    for y in range(255,0,-10):
-        shiftOut(Point(x,y))
+#reset()
+po=Point(0,0)
+pt=Point(255,255)
+pc=Point(127,127)
+#for i in xrange(0,255):
+#    p1=Point(0,i)
+#    p2=Point(i,0)
+#    l=Bezier(p1,pc,po,p2,5)
+#    for p in l.get_points():
+#        shiftOut(p)
         #time.sleep(0.01)
+
+#reset()
+c=Circle(pc,127,24)
+for i in xrange(30):
+    for p in c.get_sorted_points():
+    
+        s=Line(pc,p,12)+c
+        for p in s.get_sorted_points():
+            shiftOut(p)
+
+
 
 if not dryrunFlag:
     print("Cleaning up GPIO")
